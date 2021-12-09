54bc898692c92a2efe9f214e81137d317978307f
Keisha Gittens <keishamsa@me.com>
2021-12-08 20:26:01 -0600

    clear input field after submit

--- script.js
+++ script.js
@@ -3,14 +3,15 @@ const LETTERS = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M'
 
 
 /*----- app's state (variables) -----*/
-let wordArr = [];
 let wordToGuess;
+let word;
 let imgIndex = 0;
 let wordIndex = [];
+let wordArr = [];
+let previousGuessedLetters = [];
 
 /*----- cached element references -----*/
 const howToPlayBtn = document.querySelector('.how-to-play');
-
 const inputGroupBtns = document.querySelector('.input-section')
 const guessBoard = document.querySelector('.guessing-board');
 const previousGuessesBoard = document.querySelector('.guessed-letters');
@@ -48,7 +49,6 @@ function seperateWordInput(event) {
     }
 }
 
-
 function displayEmptyBoard() {
     for (let i = 0; i < wordArr.length; i++) {
         let spanEl = document.createElement('span');
@@ -61,33 +61,33 @@ function displayEmptyBoard() {
     }
 };
 
-
-
 function checkLetterGuess(event) {
     if (event.target.classList.contains('submit-input')) {
-        const letterInput = document.querySelector('#letter-input').value.toUpperCase();
-        addToPreviouslyGuessed(letterInput)
-        if (LETTERS.includes(letterInput)) {
-            if (wordArr.includes(letterInput)) {
-                findPositions(wordArr, letterInput)
-                let word;
+        const letterInput = document.querySelector('#letter-input');
+        let letterInputValue = letterInput.value.toUpperCase();
+        if (LETTERS.includes(letterInputValue)) {
+            if (wordArr.includes(letterInputValue)) {
+                findPositions(wordArr, letterInputValue)
                 for (let i = 0; i < wordIndex.length; i++) {
                     word = document.querySelector(`.word${wordIndex[i]}`);
                     word.style.visibility = 'visible';
                 }  
             } else {
-                console.log('here');
                 spaceshipImgs[imgIndex].style.visibility = 'visible';
                 imgIndex++;
             }
+            addToPreviouslyGuessed(letterInputValue);
+            letterInput.value = '';
         } 
     } 
 }
 
-let previousGuessedLetters = [];
 function addToPreviouslyGuessed(letter) {
     let spanEl = document.createElement('span');
     spanEl.classList.add('aside-text')
-    spanEl.innerText = letter
-    previousGuessesBoard.append(spanEl)
+    if (!previousGuessedLetters.includes(letter)) {
+        previousGuessedLetters.push(letter)
+        spanEl.innerText = letter
+        previousGuessesBoard.append(spanEl)
+    }
 }
